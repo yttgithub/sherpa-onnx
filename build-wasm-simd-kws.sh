f5c1e095080512c9c8a35a136f909a34aa45f2fb
yttgithub <97279755+yttgithub@users.noreply.github.com>
2026-04-14 19:31:13 +0800

    Update build-wasm-simd-kws.sh

--- build-wasm-simd-kws.sh
+++ build-wasm-simd-kws.sh
@@ -1,59 +1,58 @@
-#!/usr/bin/env  bash
+name: build-sherpa-onnx-wasm-kws
 
-if [ x"$EMSCRIPTEN" == x"" ]; then
-  if ! command -v emcc &> /dev/null; then
-    echo "Please install emscripten first"
-    echo ""
-    echo "You can use the following commands to install it:"
-    echo ""
-    echo "git clone https://github.com/emscripten-core/emsdk.git"
-    echo "cd emsdk"
-    echo "git pull"
-    echo "./emsdk install 3.1.53"
-    echo "./emsdk activate 3.1.53"
-    echo "source ./emsdk_env.sh"
-    exit 1
-  else
-    EMSCRIPTEN=$(dirname $(realpath $(which emcc)))
-    emcc --version
-  fi
-fi
+on:
+  workflow_dispatch:
+  push:
+    branches:
+      - main
+      - master
 
-export EMSCRIPTEN=$EMSCRIPTEN
-echo "EMSCRIPTEN: $EMSCRIPTEN"
-if [ ! -f $EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake ]; then
-  echo "Cannot find $EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake"
-  echo "Please make sure you have installed emsdk correctly"
-  echo "Hint: emsdk 3.1.53 is known to work. Other versions may not work"
-  exit 1
-fi
+jobs:
+  build-kws-wasm:
+    runs-on: ubuntu-22.04
 
-mkdir -p build-wasm-simd-kws
-pushd build-wasm-simd-kws
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v4
+        with:
+          submodules: recursive
 
-export SHERPA_ONNX_IS_USING_BUILD_WASM_SH=ON
+      - name: Install dependencies
+        run: |
+          sudo apt-get update
+          sudo apt-get install -y \
+            git \
+            cmake \
+            ninja-build \
+            python3 \
+            python3-pip \
+            curl \
+            wget \
+            tar
 
-cmake \
-  -DCMAKE_INSTALL_PREFIX=./install \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake \
-  \
-  -DSHERPA_ONNX_ENABLE_PYTHON=OFF \
-  -DSHERPA_ONNX_ENABLE_TESTS=OFF \
-  -DSHERPA_ONNX_ENABLE_CHECK=OFF \
-  -DBUILD_SHARED_LIBS=OFF \
-  -DSHERPA_ONNX_ENABLE_PORTAUDIO=OFF \
-  -DSHERPA_ONNX_ENABLE_JNI=OFF \
-  -DSHERPA_ONNX_ENABLE_C_API=ON \
-  -DSHERPA_ONNX_ENABLE_TTS=OFF \
-  -DSHERPA_ONNX_ENABLE_WEBSOCKET=OFF \
-  -DSHERPA_ONNX_ENABLE_GPU=OFF \
-  -DSHERPA_ONNX_ENABLE_WASM=ON \
-  -DSHERPA_ONNX_ENABLE_WASM_KWS=ON \
-  -DSHERPA_ONNX_ENABLE_BINARY=OFF \
-  -DSHERPA_ONNX_LINK_LIBSTDCPP_STATICALLY=OFF \
-  ..
-make -j8
-make install
+      - name: Setup emsdk 3.1.53
+        shell: bash
+        run: |
+          git clone https://github.com/emscripten-core/emsdk.git
+          cd emsdk
+          ./emsdk install 3.1.53
+          ./emsdk activate 3.1.53
 
-ls -lh install/bin/wasm
+      - name: Show emsdk version
+        shell: bash
+        run: |
+          source emsdk/emsdk_env.sh
+          emcc -v
+
+      - name: Build sherpa-onnx KWS wasm
+        shell: bash
+        run: |
+          source emsdk/emsdk_env.sh
+          chmod +x ./build-wasm-simd-kws.sh
+          ./build-wasm-simd-kws.sh
+
+      - name: Upload artifacts
+        uses: actions/upload-artifact@v4
+        with:
+          name: sherpa-onnx-wasm-kws
+          path: build-wasm-simd-kws/install/bin/wasm/
